b43dd116075f8ee0e274b093f5d6fbf6605cd5ae
CarlosLimaEstanislau <106193244+CarlosEduEstanislau@users.noreply.github.com>
2022-05-25 19:19:39 -0300

    Implementação do borad e do chess match

--- src/Aplication/App/Main.java
+++ src/Aplication/App/Main.java
@@ -1,7 +1,10 @@
 package Aplication.App;
 
+import chess.ChessMatch;
+
 public class Main {
 public static void main(String[] args) {
-        
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
     }
 }
---
+++ src/chess/ChessPiece.java
@@ -0,0 +1,21 @@
+package chess;
+
+import Aplication.Boardgame.Board;
+import Aplication.Boardgame.Piece;
+
+public class ChessPiece extends Piece{
+
+    private Color color;
+
+    public ChessPiece(Board board, Color color) {
+        super(board);
+        this.color = color;
+
+    }
+
+
+    public Color getColor() {
+        return color;
+    }
+    
+}
